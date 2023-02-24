0c114d44b40d502a8a1a5c2bd65a2f8425220a32
Hasan <projectsdata100@gmail.com>
2023-02-24 20:22:02 +0600

    First

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -6,8 +6,8 @@
 </a>-->
     <img src="{{ asset('logo/medicalteam.png') }}" width="100px">
     
-<h1>Hasan+Nabila</h1>
-<h1>I am Hasan+Nabila </h1>
+<h1>Yusuf+Nabila</h1>
+<h1>I am Yusuf+Nowshin </h1>
 <h1>Nabila</h1>
 <h1>hello </h1>
 <h1>56</h1>
