8d5812e888a30b724f183dc3d45fc7a5425dd740
Jahid01234 <mdjahidhasan790523@gmail.com>
2023-03-08 20:40:41 +0600

    Modife code

--- resources/views/user/latest.blade.php
+++ resources/views/user/latest.blade.php
@@ -6,20 +6,20 @@
           <div class="card-blog">
             <div class="header">
               <div class="post-category">
-                <a href="#">Covid19</a>
+                <a href="#">Cancer</a>
               </div>
               <a href="blog-details.html" class="post-thumb">
                 <img src="../assets/img/blog/blog_1.jpg" alt="">
               </a>
             </div>
             <div class="body">
-              <h5 class="post-title"><a href="blog-details.html">List of Countries without Coronavirus case</a></h5>
+              <h5 class="post-title"><a href="blog-details.html">Cancer refers to any one of a large number of diseases characterized</a></h5>
               <div class="site-info">
                 <div class="avatar mr-2">
                   <div class="avatar-img">
                     <img src="../assets/img/person/person_1.jpg" alt="">
                   </div>
-                  <span>Roger Adams</span>
+                  <span>Jahid Hasan</span>
                 </div>
                 <span class="mai-time"></span> 1 week ago
               </div>
@@ -30,20 +30,20 @@
           <div class="card-blog">
             <div class="header">
               <div class="post-category">
-                <a href="#">Covid19</a>
+                <a href="#">HeartAttack</a>
               </div>
               <a href="blog-details.html" class="post-thumb">
                 <img src="../assets/img/blog/blog_2.jpg" alt="">
               </a>
             </div>
             <div class="body">
-              <h5 class="post-title"><a href="blog-details.html">Recovery Room: News beyond the pandemic</a></h5>
+              <h5 class="post-title"><a href="blog-details.html">A heart attack is a life-threatening emergency. </a></h5>
               <div class="site-info">
                 <div class="avatar mr-2">
                   <div class="avatar-img">
                     <img src="../assets/img/person/person_1.jpg" alt="">
                   </div>
-                  <span>Roger Adams</span>
+                  <span>Jahid Hasan</span>
                 </div>
                 <span class="mai-time"></span> 4 weeks ago
               </div>
@@ -54,7 +54,7 @@
           <div class="card-blog">
             <div class="header">
               <div class="post-category">
-                <a href="#">Covid19</a>
+                <a href="#">Diabetes</a>
               </div>
               <a href="blog-details.html" class="post-thumb">
                 <img src="../assets/img/blog/blog_3.jpg" alt="">
@@ -67,7 +67,7 @@
                   <div class="avatar-img">
                     <img src="../assets/img/person/person_2.jpg" alt="">
                   </div>
-                  <span>Diego Simmons</span>
+                  <span>Hasan Ali </span>
                 </div>
                 <span class="mai-time"></span> 2 months ago
               </div>
